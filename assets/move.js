3e0ae352c74a81d424b33659050a9b467a2d36b5
春ノそよ風 <165978641+harunosoyokaze@users.noreply.github.com>
2024-04-04 12:33:46 +0900

    Changed scroll value

--- assets/move.js
+++ assets/move.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll',function(event)
 {
     const scrollTop = window.screenY;
 
-    if(scrollTop > 0){
+    if(scrollTop > 100){
         alert('パソコンが動作しました')
     }
 });
